1a9bd55aed451be4ecc22d16d7e17e4dd3bafe74
Matheus Miranda <mmatheus.mmiranda@gmail.com>
2026-09-01 22:42:12 -0300

    feat: adicionando post e put

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,39 +1,55 @@
 import { useTodos } from "@/hooks/useTodos";
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, Button, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const {data,isLoading,isError} = useTodos();
-  
-  if (isLoading){
-    return(
-      <View style={styles.centralizado}>
-        <ActivityIndicator size="large" color="#0000ff"/>
-        <Text>Carregadno tarefa da prova...</Text>
-      </View>
-    )
-  }
-  if (isError){
-    return(
-      <View style={styles.centralizado}>
-      <Text>Ops deu algo de errado </Text>
-      </View>
-    );
-  }
+  // Estado local para guardar o que o usuário digita
+  const [textoDigitado, setTextoDigitado] = useState('');
+
+  // Pegamos as 3 ferramentas do nosso hook
+  const {query, criarTodo, concluirTodo} = useTodos();
+  const {data, isLoading, isError} = query;
+
+  const handlerCriar = () =>{
+    if (textoDigitado.trim() === '') return;
+    criarTodo.mutate(textoDigitado);
+    setTextoDigitado(''); // Limpa o campo
+  };
+
+  if (isLoading)
+    return
+  <ActivityIndicator style={styles.centralizado} />;
+
+  if (isError)
+    return
+  <Text style={styles.centralizado}>Erro na API</Text>
+
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Tarefa (DummyJSON)</Text>
+      <Text style={styles.titulo}>Tarefa (POST e PUT)</Text>
+
+    {/* Resolução: "image_ed7c95.jpg" - Input e Botão */}
+    <View style={styles.form}>
+      <TextInput
+      style={styles.input}
+      placeholder="Digite uma nova tarefa"
+      value={textoDigitado}
+      onChangeText={setTextoDigitado}/>
+      <Button title="Cadastrar" onPress={handlerCriar} disabled={criarTodo.isPending}/>
+    </View>
 
     <FlatList
     data={data?.todos}
     keyExtractor={(item) => item.id.toString()}
     renderItem={({item}) =>(
-      <View style={styles.cartao}>
-        <Text style={[styles.texto, item.completed && styles.textoRiscado]}>
-          {item.todo}
-        </Text>
-        <Text style={styles.status}>
-        </Text>
-      </View>
+      // Resolução: "image_ed7cb5.jpg" - Transformando o cartão em clicável
+      <TouchableOpacity style={styles.cartao} onPress={() => concluirTodo.mutate(item.id)}
+      disabled={item.completed}> // Se já estiver concluído, não clica
+      <Text style={[styles.texto, item.completed && styles.textoRiscado]}>{item.todo}</Text>
+      <Text style={styles.status}>
+      {item.completed ? '✅ Concluído' : '❌ Pendente (Clique para concluir)'}
+      </Text>
+      </TouchableOpacity>
     )}
     />
     </View>
@@ -57,6 +73,21 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 15,
+  },
+
+  form:{
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+
+  input:{
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
+    marginRight: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#ccc'
   },
 
   cartao:{
--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,16 +1,66 @@
 import { TodoResponse } from "@/types/todo.response";
 import { api } from "@/utils/api";
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
+// READ
 const fetchTodos = async () => {
-//'api' pré-configurada e avisamos que o formato esperado é o 'TodoResponse'
     const resposta = await api.get<TodoResponse>('/Todos');
     return resposta.data;
 };
 
-export function useTodos(){
-    return useQuery({
-        queryKey: ['lista-todos'], // Etiqueta para o cache
-        queryFn: fetchTodos, // Função que busca os dados
+// CREATE (POST - pic)
+// A DummyJSON exige que passemos o texto, status e um userId válido
+const postTodo = async (novoTodo: string) => {
+    const resposta = await api.post('/todos/add', {
+        todo:novoTodo,
+        completed: false,
+        userId: 5,
     });
+    return resposta.data
+};
+
+// UPDATE (PUT - pic)
+// Passamos o ID na URL e avisamos que o status mudou para true
+const putTodo = async (todoId: number) =>{
+    const resposta = await api.put(`/todos/${todoId}`, {
+        completed: true,
+    });
+    return resposta.data;
+};
+
+// O Hook Principal que exportamos
+export function useTodos(){
+    const queryClient = useQueryClient();
+
+    // A busca da lista
+    const query = useQuery({
+        queryKey: ['lista-todos'],
+        queryFn: fetchTodos,
+    });
+
+    // A função de Criar
+    const criarTodo = useMutation({
+        mutationFn: postTodo,
+        // Quando criar com sucesso, avisamos o React Query para recarregar a lista
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['lista-todos'] });
+            alert("Tarefa criada com sucesso") //feedback visual
+        }
+    });
+
+    // A função de Atualizar
+    const concluirTodo =useMutation({
+        mutationFn: putTodo,
+        onSuccess: () =>{
+            queryClient.invalidateQueries({queryKey: ['lista-todos'] });
+            alert("Tarefa marcada como concluida")
+        }
+    });
+
+    // Exportamos tudo para a tela usar
+    return{
+        query,
+        criarTodo,
+        concluirTodo
+    };
 }
